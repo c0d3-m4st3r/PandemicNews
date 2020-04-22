254597e6976c7f9cf498a4e6633387ad14992034
user152439 <user152439@87-237-62-11.rdns.melbourne.co.uk>
2020-04-22 17:41:20 +0100

    Commit inicio

---
+++ PandemicNews/TabBarController.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarController.swift
+//  PandemicNews
+//
+//  Created by user152439 on 4/22/20.
+//  Copyright © 2020 FJPAFRV2020. All rights reserved.
+//
+
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
+
