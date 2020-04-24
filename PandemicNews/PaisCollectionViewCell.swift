4732e72a8795d53feab49f005037f71446f76e4d
user152439 <user152439@87-237-62-11.rdns.melbourne.co.uk>
2020-04-24 17:18:01 +0100

    archivo innecesario borrado

---
+++ PandemicNews/PaisCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PaisCollectionViewCell.swift
+//  PandemicNews
+//
+//  Created by user152439 on 4/24/20.
+//  Copyright © 2020 FJPAFRV2020. All rights reserved.
+//
+
+import UIKit
+
+class PaisCollectionViewCell: UICollectionViewCell {
+    
+}
